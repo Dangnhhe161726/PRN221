025c94a6c5a55d7ad2cfd308e28c5b2110211f62
unknown <you@example.com>
2024-02-21 00:10:47 +0700

    update

--- DocumentManagementApp/DocumentManagementApp/obj/Debug/net6.0-windows/DocumentManagementApp.AssemblyInfo.cs
+++ DocumentManagementApp/DocumentManagementApp/obj/Debug/net6.0-windows/DocumentManagementApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DocumentManagementApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d026cc744f92f33609144068a5323bebdb1067ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+486d8fb9c44add21155903ab3bf5b03f5a4b1173")]
 [assembly: System.Reflection.AssemblyProductAttribute("DocumentManagementApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DocumentManagementApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
