a0400649a45b308327995e14309c5e1473d89bf5
unknown <you@example.com>
2024-03-19 19:55:04 +0700

    push

---
+++ DocumentManager/DocumentManager/Models/AgencyIssue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DocumentManager.Models
+{
+    public partial class AgencyIssue
+    {
+        public AgencyIssue()
+        {
+            Documents = new HashSet<Document>();
+        }
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public virtual ICollection<Document> Documents { get; set; }
+    }
+}
